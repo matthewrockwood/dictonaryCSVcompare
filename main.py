26729184aa5dfc8ce1fd39e1e1957653d37698f2
matthewrockwood <rockwoodmatthew@gmail.com>
2024-03-04 09:34:20 -0500

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from openpyxl import load_workbook
 import csv
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
 
 csv_file_path = 'Data'
 # Open the CSV file in read mode
@@ -7,6 +9,7 @@ fileName = "Data.csv"
 fileName2 = "Data2.csv"
 dict = {}
 dict2 ={}
+highlight_fill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
 
 uniqueIdentifier = "CRN"
 uniqueIdentifierCol = 0
@@ -47,6 +50,7 @@ for key in dict.keys():
     if key in dict2.keys():
         if dict[key] != dict2[key]:
             print(f"Differences found for key '{key}':")
+            dict[key] += "*"
             print(f"Dictionary 1 value: {dict[key]}")
             print(f"Dictionary 2 value: {dict2[key]}")
     else:
@@ -55,5 +59,105 @@ for key in dict.keys():
 for key in dict2.keys():
     if key not in dict.keys():
         print(f"Key '{key}' only exists in Dictionary 2")
+
+wb = Workbook()
+ws = wb.active
+
+# Write headers
+headers = ['CRN', 'Data']
+ws.append(headers)
+
+# Write data from dict1 to the worksheet
+for key, value_list in dict.items():
+    # Convert each element in value_list to string and concatenate with key
+    ws.append([key] + [str(val) for val in value_list])
+
+# Save the workbook
+wb.save('dictionary_data.xlsx')
+
+wb2 = load_workbook('dictionary_data.xlsx')
+ws2 = wb2.active
+
+for row in ws2:
+    for cell in row:
+        if cell.value == '*':
+            for x in row:
+                x.fill = highlight_fill
+
+wb2.save('final.xlsx')
+fileName = "Data.csv"
+fileName2 = "Data2.csv"
+
+# Unique identifier
+uniqueIdentifier = "CRN"
+
+# Function to compare two dictionaries and highlight differences
+def compare_and_highlight(dict1, dict2):
+    highlight_fill = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
+
+    wb = Workbook()
+    ws = wb.active
+
+    # Write headers
+    headers = ['CRN', 'Data']
+    ws.append(headers)
+
+    # Iterate through the dictionaries
+    for key in dict1.keys():
+        if key in dict2.keys():
+            if dict1[key] != dict2[key]:
+                print(f"Differences found for key '{key}':")
+                for i, (val1, val2) in enumerate(zip(dict1[key], dict2[key])):
+                    if val1 != val2:
+                        dict1[key][i] = f"{val1}*"
+                        dict2[key][i] = f"{val2}*"
+                        ws.append([key] + [val1])
+                        ws.append([key] + [val2])
+                        for row in ws.iter_rows(min_row=ws.max_row-1, max_row=ws.max_row, min_col=1, max_col=len(headers)):
+                            for cell in row:
+                                if '*' in str(cell.value):
+                                    cell.fill = highlight_fill
+        else:
+            print(f"Key '{key}' only exists in Dictionary 1")
+            ws.append([key] + dict1[key])
+            for row in ws.iter_rows(min_row=ws.max_row, max_row=ws.max_row, min_col=1, max_col=len(headers)):
+                for cell in row:
+                    cell.fill = highlight_fill
+
+    for key in dict2.keys():
+        if key not in dict1.keys():
+            print(f"Key '{key}' only exists in Dictionary 2")
+            ws.append([key] + dict2[key])
+            for row in ws.iter_rows(min_row=ws.max_row, max_row=ws.max_row, min_col=1, max_col=len(headers)):
+                for cell in row:
+                    cell.fill = highlight_fill
+
+    # Save the workbook
+    wb.save('exactDifferences.xlsx')
+
+# Function to read CSV files into dictionaries
+def read_csv(file_path):
+    data_dict = {}
+    with open(file_path, 'r', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for i, row in enumerate(csv_reader):
+            if i == 0:  # Find the index of the unique identifier
+                uniqueIdentifierCol = row.index(uniqueIdentifier)
+            else:
+                data_dict[row[uniqueIdentifierCol]] = row
+    return data_dict
+
+# Read CSV files into dictionaries
+data_dict1 = read_csv(fileName)
+data_dict2 = read_csv(fileName2)
+
+# Compare dictionaries and highlight differences
+compare_and_highlight(data_dict1, data_dict2)
+
+
+
+
+
+
 
 
